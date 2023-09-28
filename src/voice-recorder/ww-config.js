e2ada0a95ee7ffddc2ce87de8149a286a7c32be7
CrabRave <martynas@idealink.tech>
2023-09-28 15:49:23 +0300

    initial

---
+++ src/voice-recorder/ww-config.js
@@ -0,0 +1,20 @@
+export default {
+    editor: {
+      label: {
+        en: "Voice Recorder",
+      },
+    },
+    properties: {
+      // If you have any customizable properties, list them here.
+      // For example:
+      recordingStatus: {
+        label: {
+          en: "Recording Status",
+        },
+        type: "Text",
+        defaultValue: "Stopped",
+      },
+      // ... add other properties if necessary
+    },
+  };
+  
---
+++ src/voice-recorder/wwElement.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <button @click="toggleRecording">
+        {{ isRecording ? 'Stop Recording' : 'Start Recording' }}
+      </button>
+      <audio v-if="audioBlob" controls :src="audioUrl"></audio>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      content: { type: "Object", required: true },
+    },
+    data() {
+      return {
+        isRecording: false,
+        audioBlob: null,
+        audioUrl: null,
+        // Initialize your recording library or functionalities here
+      };
+    },
+    methods: {
+      async toggleRecording() {
+        if (this.isRecording) {
+          // Stop recording logic here
+          this.isRecording = false;
+          // Once recording stops, save the recording to audioBlob
+          // and generate a URL for playback.
+          this.audioUrl = URL.createObjectURL(this.audioBlob);
+        } else {
+          // Start recording logic here
+          this.isRecording = true;
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
